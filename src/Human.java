8a70fed6207123f62e3155abeec59bf37094aec3
Dmitry Odinokov <ostbit@yandex.ru>
2018-11-12 00:46:14 +0300

    save

---
+++ src/Human.java
@@ -0,0 +1,16 @@
+public class Human {
+    private int age;
+    private Cat loki;
+
+    public Human(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
